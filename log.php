397a7d1121ecefdfb504c4b19d82b107efe8c09d
sp4ec <32589958+sp4ec@users.noreply.github.com>
2023-05-24 22:21:22 +0100

    Update log.php

--- log.php
+++ log.php
@@ -16,6 +16,10 @@ echo "The log.php file is working!";
 	<!-- Add any other meta tags, stylesheets, or scripts you need here -->
 </head>
 <body>
-	<!-- Add the content of your page here -->
+ <body style="background-color:black;">
+<p style="text-align:center;"><img src="https://cdn.discordapp.com/attachments/1019005728284999691/1039986543147880518/scrumptious_AdobeExpress.gif" alt="yea"></p>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6389574808588413"
+     crossorigin="anonymous"></script>
+<!DOCTYPE html>
 </body>
 </html>
